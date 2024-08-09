20d3f0a19a53857446d7e5ecd408e63cca124f5a
Ayesh Perera <givanthaperera25@gmail.com>
2024-08-10 01:56:54 +0530

    Swagger update

--- MyProgressTrackerAuthenticationService/Program.cs
+++ MyProgressTrackerAuthenticationService/Program.cs
@@ -24,15 +24,10 @@ namespace MyProgressTrackerAuthenticationService
             builder.Services.AddScoped<UserRegistrationHandler>();
 
             var app = builder.Build();
+			app.UseSwagger();
+			app.UseSwaggerUI();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
+			app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
